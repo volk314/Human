309e5a880f8619fae13c9fdbfe8317768f3639b9
avolkdev <anastasiia.volkova@outlook.com>
2020-02-08 22:51:03 +0200

    task 2 initial commit

--- Human/Model/Logic.swift
+++ Human/Model/Logic.swift
@@ -10,12 +10,10 @@ import Foundation
 
 class Logic{
     func main() -> String{
-        let human = Human.init(a: 21, h: 172, w: 67)
-        let child = Child.init(a: 14, h: 156, w: 49, f: "Peter", l: "Malkowski")
-        let humanS = HumanStruct(a: 28, h: 151, w: 52)
-        let childS = ChildStruct(a: 90, h: 180, w: 80, f: "Olivia", l: "Dahnem")
+        let human = HumanStruct(a: 28, h: 151, w: 52)
+        let child = ChildStruct(a: 90, h: 180, w: 80, f: "Olivia", l: "Dahnem")
         
-        return "\(human.getResult())\n\(child.getResult())\n\(humanS.getResult())\n\(childS.getResult())"
+        return "\(human.getResult())\n\(child.getResult())"
         
     }
 }
